2fdcba8d28e3b4da3aa8cecc78e3218a329fb978
kamilzielinskidev <kamilzielinskidev@gmail.com>
2020-12-16 21:09:17 +0100

    feat: task 13 solution

---
+++ 13/solution.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from "ts-node";
+
+const main = () => {
+  const data = fs.readFileSync(path.join(__dirname, "./data"), "utf8");
+  const [dataTimestamp, dataBuses] = data.split("\n");
+  const timestamp = parseInt(dataTimestamp);
+  const buses = dataBuses
+    .split(",")
+    .filter((busPosition) => busPosition !== "x")
+    .map((bus) => parseInt(bus));
+
+  const minutesFromDepartingBuses = buses.map((bus) => bus - (timestamp % bus));
+  const closesBusIndex = minutesFromDepartingBuses.findIndex(
+    (val, _, arr) => val === Math.min(...arr)
+  );
+
+  console.log(
+    buses[closesBusIndex] * minutesFromDepartingBuses[closesBusIndex]
+  );
+};
+
+main();
